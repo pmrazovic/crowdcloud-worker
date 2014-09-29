335e6866b6b53aa67709786be73a1ad4f3ccb2f9
Petar Mrazovic <petar.mrazovic@inge-mark.com>
2014-09-29 13:57:18 +0200

    Working on visual design.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,14 +3,49 @@ angular.module('starter.controllers', [])
 .controller('AppController', function($scope) {
 })
 
-.controller('DashboardController', function($scope, $ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    $scope.test = 'Device Name: '     + device.name     + '<br />' +
-                  'Device Cordova: '  + device.cordova  + '<br />' +
-                  'Device Platform: ' + device.platform + '<br />' +
-                  'Device UUID: '     + device.uuid     + '<br />' +
-                  'Device Model: '    + device.model    + '<br />' +
-                  'Device Version: '  + device.version  + '<br />';
-  });
+.controller('DashboardController', function($scope, $http, $ionicLoading, $ionicPopup) {
+
+  $scope.showLoading = function() {
+    $ionicLoading.show({
+      template: '<i class="icon ion-loading-b"></i> Loading...'
+    });
+  };
+
+  $scope.hideLoading = function(){
+    $ionicLoading.hide();
+  };
+
+  $scope.register = function() {
+    $scope.showLoading();
+    var _params = { uuid:     device.uuid,
+                    name:     device.name,
+                    platform: device.platform,
+                    model:    device.model,
+                    version:  device.version }
+    $http.post("http://192.168.1.78:3000/devices/register", _params, {headers: {'Accept' : 'application/json; charset=UTF-8'}})
+      .success(function (data) {
+        var successPopup = $ionicPopup.alert({
+          title: 'Successful registration',
+          template: 'Your device is successfuly registered in CrowdCloud!',
+          buttons: [{
+              text: 'OK',
+              type: 'button-dark'
+            }]
+        });
+      })
+      .error(function (data) {
+        var errorPopup = $ionicPopup.alert({
+          title: 'Registration error',
+          template: 'Your device could not be registered in CrowdCloud!',
+          buttons: [{
+              text: 'OK',
+              type: 'button-dark'
+            }]
+        });
+      })
+      .finally(function() {
+        $scope.hideLoading();
+      });
+  };
 
 })
